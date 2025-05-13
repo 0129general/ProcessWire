7fd6327c03d8e5c0a4b6dda7b4dada90db656c82
BestLife <meromantic129@gmail.com>
2025-05-13 18:43:07 +0200

    update the work section of current project page

--- site/views/partials/current-project/work.php
+++ site/views/partials/current-project/work.php
@@ -1,30 +1,18 @@
-<?php
-$assets = $config->urls->templates . 'assets/'; // Asset path
-$workImage = 'images/projects/work-image-s3.jpg'; // Image filename
-$sectionTitle = 'How We Manage Renovations'; // H2 title
-$steps = [
-    'Consultation & Deal',
-    'Design & Planning',
-    'Start Working',
-    'Renovation',
-    'Completion'
-];
-?>
-
-<!--===== start wpo-work-section =====-->
 <section class="wpo-work-section-s3">
     <div class="container-fluid p-0">
         <div class="wraper">
             <div class="left-content">
-                <h2 class="poort-text poort-in-right"><?= $sectionTitle ?></h2>
+                <h2 class="poort-text poort-in-right"><?= $page->work_title ?></h2>
                 <ul class="scroll-text-animation" data-animation="fade_from_right">
-                    <?php foreach ($steps as $index => $step): ?>
-                        <li><a href="#"><?= ($index + 1) ?>. <?= $step ?></a></li>
+                    <?php foreach ($page->work_steps as $i => $step): ?>
+                        <li><a href="#"><?= ($i + 1) ?>. <?= $step->step_label ?></a></li>
                     <?php endforeach; ?>
                 </ul>
             </div>
             <div class="right-image">
-                <img src="<?= $assets . $workImage ?>" alt="Work Step Image">
+                <?php if ($page->work_image): ?>
+                    <img src="<?= $page->work_image->url ?>" alt="Work Step Image">
+                <?php endif; ?>
             </div>
         </div>
     </div>
