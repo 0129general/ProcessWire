b6f2bf7d23016ff95b6f1a2161acdfd9b93128b1
BestLife <meromantic129@gmail.com>
2025-05-13 16:54:09 +0200

    update the founder section of home page with Profieds

--- site/views/partials/home/founder.php
+++ site/views/partials/home/founder.php
@@ -1,44 +1,43 @@
-  <!--===== start wpo-page-title =====-->
-  <section class="wpo-page-title">
-      <div class="container">
-          <div class="row">
-              <div class="col col-xs-12">
-                  <div class="wpo-breadcumb-wrap">
-                      <ol>
-                          <li><img src="<?= $assets ?>/images/logo-skks.png" alt="logo"></li>
-                      </ol>
-                      <h2 class="poort-text poort-in-right">Willkommen daheim!</h2>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </section>
+<section class="wpo-page-title">
+    <div class="container">
+        <div class="row">
+            <div class="col col-xs-12">
+                <div class="wpo-breadcumb-wrap">
+                    <ol>
+                        <?php if ($page->founder_logo): ?>
+                            <li>
+                                <img src="<?= $page->founder_logo->url ?>" alt="logo">
+                            </li>
+                        <?php endif; ?>
+                    </ol>
+                    <h2 class="poort-text poort-in-right"><?= $page->founder_title ?></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
-  <!--===== start wpo-service-section =====-->
-  <section class="wpo-blog-pg-section blog-pg-fullwidth section-padding pt-0">
-      <div class="container-fluid">
-          <div class="wpo-blog-wraper">
-              <div class="wpo-blog-content">
-                  <div class="post format-standard-image scroll-text-animation" data-animation="fade_from_bottom">
-                      <div class="entry-media">
-                          <img src="<?= $assets ?>/images/home/founder.jpg" alt="logo">
-                      </div>
-                      <div class="entry-details">
-                          <div class="entry-meta">
-                              <ul>
-                                  <!-- <li>By:<a href="#">admin </a> </li> -->
-                                  <li>Klaus Stora & Sebastian Kröger</li>
-                              </ul>
-                          </div>
-                          <h3><a href="#">SKKS-Nordheide GbR</a>
-                          </h3>
-                          <p>Ihre Zufriedenheit ist unsere Motivation. Darum sorgen wir vom ersten Tag der Planung an dafür, dass unser Bauprojekt all Ihre Erwartungen erfüllt. Solide Bauweise, gut funktionierende Grundrisse, modernes Design, hochwertige Ausstattung und unbedingte Energieeffizienz: Für Ihren Komfort gehen wir weiter als andere. Wir sind überzeugt, dass Sie sich in Ihrem neuen Eigenheim auf Anhieb zu Hause fühlen. Melden Sie sich gern, wir freuen uns auf Sie!
-
-                              Ihre</p>
-                          <!-- <a href="blog-single.html" class="read-more">Read More..</a> -->
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </div>
-  </section>
+<section class="wpo-blog-pg-section blog-pg-fullwidth section-padding pt-0">
+    <div class="container-fluid">
+        <div class="wpo-blog-wraper">
+            <div class="wpo-blog-content">
+                <div class="post format-standard-image scroll-text-animation" data-animation="fade_from_bottom">
+                    <div class="entry-media">
+                        <?php if ($page->founder_image): ?>
+                            <img src="<?= $page->founder_image->url ?>" alt="founder">
+                        <?php endif; ?>
+                    </div>
+                    <div class="entry-details">
+                        <div class="entry-meta">
+                            <ul>
+                                <li><?= $page->founder_names ?></li>
+                            </ul>
+                        </div>
+                        <h3><a href="#"><?= $page->founder_company ?></a></h3>
+                        <p><?= $page->founder_paragraph ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
