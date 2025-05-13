7d1bf6616b7b5c0dfe52afac4a229683943daf59
BestLife <meromantic129@gmail.com>
2025-05-13 03:09:47 +0200

    update the partner logos using Profields feature

--- site/views/partials/partners.php
+++ site/views/partials/partners.php
@@ -1,15 +1,6 @@
 <section class="wpo-partners-section fade_bottom">
   <h2 class="d-none">No Content</h2>
-  <ul class="partners-slider">
-    <?php
-    $partners = ['1.png', '2.png', '3.png', '4.png', '5.png', '2.png'];
-    foreach ($partners as $logo):
-    ?>
-      <li>
-        <div>
-          <img src="<?= $assets ?>/images/<?= $logo ?>" alt="partner logo">
-        </div>
-      </li>
-    <?php endforeach; ?>
-  </ul>
+  <ul class="partners-slider"> <?php foreach ($page->partner_logos as $item): ?> <li>
+        <div> <img src="<?= $item->partner_logo_image->first()->url ?>" alt="partner logo"> </div>
+      </li> <?php endforeach; ?> </ul>
 </section>
