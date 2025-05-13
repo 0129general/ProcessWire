7ac189b5c725ab9ea34fce622418e532f51933bd
BestLife <meromantic129@gmail.com>
2025-05-13 04:23:33 +0200

    update the partner section without the errors

--- site/views/partials/partners.php
+++ site/views/partials/partners.php
@@ -1,6 +1,20 @@
 <section class="wpo-partners-section fade_bottom">
   <h2 class="d-none">No Content</h2>
-  <ul class="partners-slider"> <?php foreach ($page->partner_logos as $item): ?> <li>
-        <div> <img src="<?= $item->partner_logo_image->first()->url ?>" alt="partner logo"> </div>
-      </li> <?php endforeach; ?> </ul>
+  <ul class="partners-slider">
+    <?php foreach ($page->partner_logos as $item): ?>
+      <li>
+        <div>
+          <?php if ($item->partner_url): ?>
+            <a href="<?= $item->partner_url ?>" target="_blank" rel="noopener">
+            <?php endif; ?>
+
+            <img src="<?= $item->partner_logo_image->url ?>" alt="<?= $item->partner_name ?: 'partner logo' ?>">
+
+            <?php if ($item->partner_url): ?>
+            </a>
+          <?php endif; ?>
+        </div>
+      </li>
+    <?php endforeach; ?>
+  </ul>
 </section>
