b74b2569a7e54b0b8160916a2c8f688279362f1e
BestLife <meromantic129@gmail.com>
2025-05-14 07:28:08 +0200

    update the team of about page with Profields

--- site/views/partials/about/team.php
+++ site/views/partials/about/team.php
@@ -1,49 +1,27 @@
-<?php
-// Define an array for team members' data
-$team_members = [
-    [
-        'name' => 'Wade Warren',
-        'role' => 'Senior Architect',
-        'image' => '/images/about/arch_1.jpg',
-        'link' => 'team-single.html'
-    ],
-    [
-        'name' => 'Esther Howard',
-        'role' => 'Senior Architect',
-        'image' => '/images/about/arch_2.jpg',
-        'link' => 'team-single.html'
-    ],
-    [
-        'name' => 'Robert Willum',
-        'role' => 'Senior Architect',
-        'image' => '/images/about/arch_3.jpg',
-        'link' => 'team-single.html'
-    ]
-];
-
-$ourteam = "Our Team";
-$title = "Our Expert Architect";
-?>
-
-<!--===== start wpo-team-section =====-->
 <section class="wpo-team-section-s12 section-padding">
     <div class="container">
         <div class="title">
-            <h2 class="poort-text poort-in-right"><?= $ourteam ?></h2>
-            <h3 class="poort-text poort-in-right"><?= $title ?></h3>
+            <h2 class="poort-text poort-in-right"><?= $page->team_s12_heading ?></h2>
+            <h3 class="poort-text poort-in-right"><?= $page->team_s12_subtitle ?></h3>
         </div>
         <div class="team-wrap">
             <div class="row">
-                <?php foreach ($team_members as $member): ?>
+                <?php foreach ($page->team_s12_members as $member): ?>
                     <div class="col-lg-4 col-md-6 col-12 fade_bottom">
                         <div class="team-card-s12">
                             <div class="image">
-                                <img src="<?= $assets . $member['image'] ?>" alt="team">
-                                <img src="<?= $assets . $member['image'] ?>" alt="team">
+                                <?php if ($member->team_member_image): ?>
+                                    <img src="<?= $member->team_member_image->url ?>" alt="team">
+                                    <img src="<?= $member->team_member_image->url ?>" alt="team">
+                                <?php endif; ?>
                             </div>
                             <div class="text">
-                                <h2><a href="<?= $member['link'] ?>"> <span class="rolling-text"><?= $member['name'] ?></span></a></h2>
-                                <span><?= $member['role'] ?></span>
+                                <h2>
+                                    <a href="<?= $member->team_member_link ?>">
+                                        <span class="rolling-text"><?= $member->team_member_name ?></span>
+                                    </a>
+                                </h2>
+                                <span><?= $member->team_member_role ?></span>
                             </div>
                         </div>
                     </div>
@@ -52,4 +30,3 @@ $title = "Our Expert Architect";
         </div>
     </div>
 </section>
-<!--===== end wpo-team-section =====-->
