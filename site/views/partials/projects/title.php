80fa906f8f2ab568c37422900fd0a0a7984d9ca5
BestLife <meromantic129@gmail.com>
2025-05-14 14:53:36 +0200

    update the title section of projects page with Profields

--- site/views/partials/projects/title.php
+++ site/views/partials/projects/title.php
@@ -1,21 +1,12 @@
-<?php
-// Define the breadcrumb and title text as variables
-$breadcrumbText = 'Our Projects';
-$pageTitle = 'Interior Designer, Expert Architect';
-?>
-
-<!--===== start wpo-page-title =====-->
 <section class="wpo-page-title">
     <div class="container">
         <div class="row">
             <div class="col col-xs-12">
                 <div class="wpo-breadcumb-wrap">
                     <ol>
-                        <!-- Use the breadcrumb variable -->
-                        <li><?= $breadcrumbText ?></li>
+                        <li><?= $page->projects_breadcrumb ?></li>
                     </ol>
-                    <!-- Use the title variable -->
-                    <h2 class="poort-text poort-in-right"><?= $pageTitle ?></h2>
+                    <h2 class="poort-text poort-in-right"><?= $page->projects_title ?></h2>
                 </div>
             </div>
         </div>
