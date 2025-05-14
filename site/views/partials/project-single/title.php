7e90c2325e770fa1e3d4d1665cf9fb40c40fe9aa
BestLife <meromantic129@gmail.com>
2025-05-14 15:44:26 +0200

    update the title of single project with profields

--- site/views/partials/project-single/title.php
+++ site/views/partials/project-single/title.php
@@ -1,18 +1,12 @@
-<?php
-$pageTitle = "Modern Loft Apartment Design";
-$breadcrumb = "Project Single";
-?>
-
-<!--===== start wpo-page-title =====-->
 <section class="wpo-page-title">
     <div class="container">
         <div class="row">
             <div class="col col-xs-12">
                 <div class="wpo-breadcumb-wrap">
                     <ol>
-                        <li><?= $breadcrumb ?></li>
+                        <li><?= $page->single_project_breadcrumb ?></li>
                     </ol>
-                    <h2 class="poort-text poort-in-right"><?= $pageTitle ?></h2>
+                    <h2 class="poort-text poort-in-right"><?= $page->single_project_page_title ?></h2>
                 </div>
             </div>
         </div>
