87518226ce11b7a9683dfb911b0fe151d7d837e3
BestLife <meromantic129@gmail.com>
2025-05-14 07:55:26 +0200

    update the title of contact page with Profields

--- site/views/partials/contact/title.php
+++ site/views/partials/contact/title.php
@@ -1,19 +1,12 @@
-<?php
-// Dynamic Page Title and Breadcrumb
-$pageTitle = "Contact Us";  // Example dynamic title
-$breadcrumb = "Contact Us"; // Dynamic breadcrumb
-?>
-
-<!--===== start wpo-page-title =====-->
 <section class="wpo-page-title">
     <div class="container">
         <div class="row">
             <div class="col col-xs-12">
                 <div class="wpo-breadcumb-wrap">
                     <ol>
-                        <li><?= $breadcrumb ?></li>
+                        <li><?= $page->breadcrumb_text ?></li>
                     </ol>
-                    <h2 class="d-none"><?= $pageTitle ?></h2>
+                    <h2 class="d-none"><?= $page->page_heading ?></h2>
                 </div>
             </div>
         </div>
