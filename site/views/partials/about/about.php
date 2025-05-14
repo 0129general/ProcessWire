b54fba2422efd580c40061ed6f69a5abaea9c84d
BestLife <meromantic129@gmail.com>
2025-05-14 06:17:38 +0200

    update the about us of about page with Profields

--- site/views/partials/about/about.php
+++ site/views/partials/about/about.php
@@ -1,41 +1,36 @@
-<?php
-// Define PHP variables for dynamic content
-$aboutImage = "$assets/images/about/construct.png"; // Image source
-$aboutTitle = "About Us";
-$aboutSubtitle = "Bliize is An Award Winning Construction Company";
-$aboutText = "Blaze is a trailblazing architecture agency renowned for its innovative approach to design, where creativity meets functionality. From futuristic skyscrapers to timeless cultural landmarks, We specialize in crafting spaces that inspire and endure. Blaze pushes boundaries, transforming bold concepts into architectural masterpieces.";
-$currentClients = 215;
-$projectsComplete = 550;
-$currentClientsLabel = "CURRENT CLIENTS";
-$projectsCompleteLabel = "PROJECTS COMPLETE";
-?>
-
-<!--===== start wpo-about-section-s12 =====-->
 <section class="wpo-about-section-s12 section-padding">
     <div class="wraper">
         <div class="row align-items-center">
             <div class="col-lg-6 col-12">
                 <div class="about-left">
-                    <img src="<?= $aboutImage ?>" alt="About Image">
+                    <?php if ($page->about_image): ?>
+                        <img src="<?= $page->about_image->url ?>" alt="About Image">
+                    <?php endif; ?>
                 </div>
             </div>
             <div class="col-lg-6 col-12">
                 <div class="about-right">
-                    <h2 class="fade_bottom"><?= $aboutTitle ?></h2>
-                    <h3 class="fade_bottom"><?= $aboutSubtitle ?></h3>
-                    <p><?= $aboutText ?></p>
+                    <h2 class="fade_bottom"><?= $page->about_us_title ?></h2>
+                    <h3 class="fade_bottom"><?= $page->about_subtitle ?></h3>
+                    <p><?= $page->about_description ?></p>
                     <div class="fanfuct">
                         <div class="row">
                             <div class="col-md-6 col-12 fade_bottom">
                                 <div class="item">
-                                    <h4><span class="odometer" data-count="<?= $currentClients ?>">00</span> <span class="icon">+</span></h4>
-                                    <h5><?= $currentClientsLabel ?></h5>
+                                    <h4>
+                                        <span class="odometer" data-count="<?= $page->about_stat_1_count ?>">00</span>
+                                        <span class="icon">+</span>
+                                    </h4>
+                                    <h5><?= $page->about_stat_1_label ?></h5>
                                 </div>
                             </div>
                             <div class="col-md-6 col-12 fade_bottom">
                                 <div class="item">
-                                    <h4><span class="odometer" data-count="<?= $projectsComplete ?>">00</span> <span class="icon">+</span></h4>
-                                    <h5><?= $projectsCompleteLabel ?></h5>
+                                    <h4>
+                                        <span class="odometer" data-count="<?= $page->about_stat_2_count ?>">00</span>
+                                        <span class="icon">+</span>
+                                    </h4>
+                                    <h5><?= $page->about_stat_2_label ?></h5>
                                 </div>
                             </div>
                         </div>
@@ -45,4 +40,3 @@ $projectsCompleteLabel = "PROJECTS COMPLETE";
         </div>
     </div>
 </section>
-<!--===== end wpo-about-section-s12 =====-->
